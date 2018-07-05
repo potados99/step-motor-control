a2d9866f061c3e98c02b279125ea0f970da1f5e7
potados99 <potados99@gmail.com>
2018-07-05 18:38:44 +0900

    Make it an instrument

--- step-motor-control.ino
+++ step-motor-control.ino
@@ -3,14 +3,19 @@
 #define _ROTATION_CW 0
 #define _ROTATION_CCW 1
 #define _STEP_ANGLE (1.8)
-#define _STEP_RATE (int)(360 / _STEP_ANGLE * 2)
+#define _STEP_RATE (360 / _STEP_ANGLE * 2)
 #define _MAX_SPEED 2100
 
 
 
 void main_routine() {
-  for (unsigned register int i = 400; i < _MAX_SPEED; i += 100) {
-    rotate(1, i, _ROTATION_CCW);
+
+  for (unsigned register int i = 1; i < _MAX_SPEED; i += 1) {
+    rotate(0.01, i, _ROTATION_CCW);
+  }
+
+  for (unsigned register int i = _MAX_SPEED; i >= 1000; i -= 1) {
+    rotate(0.01, i, _ROTATION_CCW);
   }
   
   delay(500);
@@ -18,6 +23,12 @@ void main_routine() {
   for (unsigned register int i = _MAX_SPEED - 600; i > 300 ; i -= 100) {
     rotate(1, i, (i/100) % 2);
   }
+ 
+
+  for (unsigned register int i = 0; i < 8; ++ i) {
+    rotate((double)_c_scale()[i] / 4000, _c_scale()[i], _ROTATION_CCW);
+  }
+  
 }
 
 
@@ -44,7 +55,7 @@ void init_pins() {
   pinMode(_MOTOR_DIRECTION_PIN_NUMBER, OUTPUT);
 }
 
-void rotate(int _rotations, double _speed, int _direction) {
+void rotate(double _rotations, double _speed, int _direction) {
   // speed: How many clocks in 1000 milli seconds (400 clocks in one rotations)
 
   _set_rotation_direction(_MOTOR_DIRECTION_PIN_NUMBER, _direction);
@@ -67,4 +78,8 @@ void _set_rotation_direction(int _pinNumber, int _direction) {
   digitalWrite(_pinNumber, _direction);
 }
 
+int *_c_scale() {
+  static int scale[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  return scale;
+}
 
