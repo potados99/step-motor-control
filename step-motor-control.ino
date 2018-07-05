0449d379a0ad69b6d9200a5ddfa5e2816e577f0d
potados99 <potados99@gmail.com>
2018-07-05 17:03:41 +0900

    Add source file

---
+++ step-motor-control.ino
@@ -0,0 +1,70 @@
+#define _MOTOR_CLOCK_PIN_NUMBER 12
+#define _MOTOR_DIRECTION_PIN_NUMBER 13
+#define _ROTATION_CW 0
+#define _ROTATION_CCW 1
+#define _STEP_ANGLE (1.8)
+#define _STEP_RATE (int)(360 / _STEP_ANGLE * 2)
+#define _MAX_SPEED 2100
+
+
+
+void main_routine() {
+  for (unsigned register int i = 400; i < _MAX_SPEED; i += 100) {
+    rotate(1, i, _ROTATION_CCW);
+  }
+  
+  delay(500);
+  
+  for (unsigned register int i = _MAX_SPEED - 600; i > 300 ; i -= 100) {
+    rotate(1, i, (i/100) % 2);
+  }
+}
+
+
+
+void setup() {
+  init_pins();
+}
+
+void loop() {
+  unsigned const infiniteLoop = false;
+  unsigned const int repeats = 1;
+  
+  unsigned static int _count = 0;
+  if (! infiniteLoop && _count >= repeats) return;
+
+  main_routine();
+
+  ++ _count;
+}
+
+
+void init_pins() {
+  pinMode(_MOTOR_CLOCK_PIN_NUMBER, OUTPUT);
+  pinMode(_MOTOR_DIRECTION_PIN_NUMBER, OUTPUT);
+}
+
+void rotate(int _rotations, double _speed, int _direction) {
+  // speed: How many clocks in 1000 milli seconds (400 clocks in one rotations)
+
+  _set_rotation_direction(_MOTOR_DIRECTION_PIN_NUMBER, _direction);
+
+  double _clockCycle = 1000 / _speed;
+  
+  for (unsigned register int i = 0; i < _rotations * _STEP_RATE; ++ i) {
+     _do_one_clock(_MOTOR_CLOCK_PIN_NUMBER, _clockCycle);
+  }
+}
+
+void _do_one_clock(int _pinNumber, double _clockCycle) {
+  digitalWrite(_pinNumber, HIGH);
+  delayMicroseconds(_clockCycle * 500);
+  digitalWrite(_pinNumber, LOW);
+  delayMicroseconds(_clockCycle * 500);
+}
+
+void _set_rotation_direction(int _pinNumber, int _direction) {
+  digitalWrite(_pinNumber, _direction);
+}
+
+
